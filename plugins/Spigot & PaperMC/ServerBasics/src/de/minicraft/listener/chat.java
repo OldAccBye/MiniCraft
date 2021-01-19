596b18dee2ac3035161b30b54a7d23f514c509a5
Nick <44214829+EntenKoeniq@users.noreply.github.com>
2021-01-19 05:22:11 +0100

    UPDATE #5
    Replaced loops for better performance

--- plugins/Spigot & PaperMC/ServerBasics/src/de/minicraft/listener/chat.java
+++ plugins/Spigot & PaperMC/ServerBasics/src/de/minicraft/listener/chat.java
@@ -1,9 +1,8 @@
 package de.minicraft.listener;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -16,22 +15,23 @@ import de.minicraft.config;
 import de.minicraft.players.playerApi;
 
 public class chat implements Listener {
-    private String tempMsg;
-
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e) {
+        String tempMsg = e.getMessage();
         Player p = e.getPlayer();
 
         e.setCancelled(true);
 
-        tempMsg = e.getMessage();
+        if (tempMsg.contains("@")) {
+            String t = tempMsg.substring(tempMsg.indexOf("@") + 1).split(" ")[0];
 
-        p.getWorld().getPlayers().stream().filter(players -> tempMsg.contains("@" + p.getName())).forEach(players -> {
-            //if (players.getName() != p.getName())
-            players.sendMessage(config.getLanguageText(p.getUniqueId(), "playerHasBeenMarked").replace("%username%", p.getName()));
-
-            tempMsg = tempMsg.replace("@" + players.getName(), "§b@" + players.getName() + "§r");
-        });
+            if (Bukkit.getPlayerExact(t) == null)
+                p.sendMessage(config.getLanguageText(p.getUniqueId(), "playerNotFound"));
+            else if (!t.equals(p.getName())) {
+                p.sendMessage(config.getLanguageText(p.getUniqueId(), "playerHasBeenMarked").replace("%username%", p.getName()));
+                tempMsg = tempMsg.replace("@" + p.getName(), "§b@" + p.getName() + "§r");
+            }
+        }
 
         for (Player t : p.getWorld().getPlayers())
             t.sendMessage(config.getLanguageText(p.getUniqueId(), "prefix." + Objects.requireNonNull(playerApi.getPlayer(p.getUniqueId())).group) + p.getName() + ": " + tempMsg);
@@ -41,37 +41,24 @@ public class chat implements Listener {
     public void onCommand(PlayerCommandPreprocessEvent e) {
         Player p = e.getPlayer();
 
-        for (String cmd : config.commandList.getKeys(false)) {
-            if (!e.getMessage().startsWith("/" + cmd)) continue;
-
-            if (!p.hasPermission(Objects.requireNonNull(config.commandList.getString(cmd)))) {
-                e.setCancelled(true);
-                p.sendMessage(config.getLanguageText(p.getUniqueId(), "noPermission"));
-            }
-
+        String firstWord = e.getMessage().split(" ")[0].replace("/", "");
+        if (!config.commandList.getKeys(true).contains(firstWord)) {
+            e.setCancelled(true);
+            p.sendMessage(config.getLanguageText(p.getUniqueId(), "cmdNotExists"));
             return;
         }
 
-        e.setCancelled(true);
-        p.sendMessage(config.getLanguageText(p.getUniqueId(), "cmdNotExists"));
+        if (!p.hasPermission(Objects.requireNonNull(config.commandList.getString(firstWord)))) {
+            e.setCancelled(true);
+            p.sendMessage(config.getLanguageText(p.getUniqueId(), "noPermission"));
+        }
     }
 
     @EventHandler
     public void onCommandTabSend(PlayerCommandSendEvent e) {
-        // Normal commands
-        List<String> blockedCommands = Arrays.asList("?", "about", "help", "icanhasbukkit", "list", "me", "msg", "pl", "plugins", "teammsg", "tell", "tm", "trigger", "ver", "version", "w");
-        e.getCommands().removeAll(blockedCommands);
+        e.getCommands().clear();
 
-        // Minecraft commands
-        List<String> blockedMinecraftCommands = Arrays.asList("minecraft:help", "minecraft:list", "minecraft:me", "minecraft:msg", "minecraft:teammsg", "minecraft:tell", "minecraft:tm", "minecraft:trigger", "minecraft:w");
-        e.getCommands().removeAll(blockedMinecraftCommands);
-
-        // Bukkit commands
-        List<String> blockedBukkitCommands = Arrays.asList("bukkit:?", "bukkit:about", "bukkit:help", "bukkit:pl", "bukkit:plugins", "bukkit:ver", "bukkit:version");
-        e.getCommands().removeAll(blockedBukkitCommands);
-
-        // "serverbasics:*" commands
         for (String cmd : config.commandList.getKeys(false))
-            e.getCommands().remove("serverbasics:" + cmd);
+            e.getCommands().add(cmd);
     }
 }
