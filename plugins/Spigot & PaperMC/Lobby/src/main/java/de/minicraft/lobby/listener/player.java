9c002349ee6d668818744f7fb8ae2a335097d268
Nick <44214829+EntenKoeniq@users.noreply.github.com>
2021-02-01 11:59:33 +0100

    Lobby UPDATE

--- plugins/Spigot & PaperMC/Lobby/src/main/java/de/minicraft/lobby/listener/player.java
+++ plugins/Spigot & PaperMC/Lobby/src/main/java/de/minicraft/lobby/listener/player.java
@@ -1,34 +1,45 @@
 package de.minicraft.lobby.listener;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class player implements Listener {
+    private final HashMap<UUID, Boolean> doubleJump = new HashMap<>();
+
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        e.getPlayer().getInventory().clear();
-
-        ItemStack i = new ItemStack(Material.COMPASS);
-        ItemMeta im = i.getItemMeta();
-        if (im == null) {
-            e.getPlayer().kickPlayer("Something gone wrong! please try to login again.");
-            return;
+        // Inventory
+        {
+            e.getPlayer().getInventory().clear();
+            ItemStack i = new ItemStack(Material.COMPASS);
+            ItemMeta im = i.getItemMeta();
+            if (im == null) {
+                e.getPlayer().kickPlayer("Something gone wrong! please try to login again.");
+                return;
+            }
+            im.setDisplayName("§3§lNavigator");
+            i.setItemMeta(im);
+            e.getPlayer().getInventory().setItem(4, i);
         }
-        im.setDisplayName("§3§lNavigator");
-        i.setItemMeta(im);
-        e.getPlayer().getInventory().setItem(4, i);
+
+        // Double jump
+        {
+            e.getPlayer().setAllowFlight(true);
+            doubleJump.put(e.getPlayer().getUniqueId(), false);
+        }
     }
 
     @EventHandler
@@ -65,4 +76,29 @@ public class player implements Listener {
 
     @EventHandler
     public void onPlayerDropItem(PlayerDropItemEvent e) { e.setCancelled(true); }
+
+    @EventHandler
+    public void onFly(PlayerToggleFlightEvent e) {
+        if (e.getPlayer().getGameMode() != GameMode.SURVIVAL) return;
+
+        e.setCancelled(true);
+
+        if (doubleJump.get(e.getPlayer().getUniqueId())) return;
+        e.getPlayer().setVelocity(e.getPlayer().getLocation().getDirection().setY(1));
+        doubleJump.computeIfPresent(e.getPlayer().getUniqueId(), (k, v) -> v = true);
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        if (!e.getPlayer().getLocation().subtract(0, 0.1, 0).getBlock().getType().isSolid()) return;
+
+        doubleJump.computeIfPresent(e.getPlayer().getUniqueId(), (k, v) -> v = false);
+    }
+
+    @EventHandler
+    public void onGamemodeChange(PlayerGameModeChangeEvent e) {
+        if (e.getNewGameMode() != GameMode.SURVIVAL) return;
+
+        e.getPlayer().setAllowFlight(true);
+    }
 }
