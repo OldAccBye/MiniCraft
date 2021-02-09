5dcee4da84c8a142c12a0e06aa84b1a10cadbf9b
Nick <44214829+EntenKoeniq@users.noreply.github.com>
2021-02-09 06:49:27 +0100

    Web UPDATE

--- Web/app.js
+++ Web/app.js
@@ -1,9 +1,6 @@
-const path = require('path'), express = require('express'), app = express(), router = express.Router(), PORT = process.env.PORT || 80;
-const mongoose = require('mongoose'), db = mongoose.connection;
-
-// Models
-const players = require('./models/players');
-const playersFFA = require('./models/ffa');
+const path = require('path'), express = require('express'), app = express(), PORT = process.env.PORT || 80;
+const mongoose = require('mongoose');
+const getRouter = require('./routes/get');
 
 // Connect to mongodb
 mongoose.connect('mongodb+srv://miniuser:minipass@minicraft.kxkh9.mongodb.net/MiniCraft?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -15,58 +12,5 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + '/public'));
 
-// Router get
-router.get('/', async (req, res) => {
-    res.render('index', { userCount: await players.countDocuments() });
-});
-
-router.get('/team', async (req, res) => {
-    res.render('team', { userCount: await players.countDocuments() });
-});
-
-router.get('/top', async (req, res) => {
-    const FFAPlayer = await playersFFA.find().sort({ kills: -1 }).limit(1);
-    const player = await players.findOne({ UUID: FFAPlayer[0].UUID });
-
-    if (FFAPlayer === null || player === null) {
-        res.render('topPlayers', { userCount: await players.countDocuments() })
-        return;
-    }
-
-    res.render('topPlayers', { userCount: await players.countDocuments(), ffa: { uuid: FFAPlayer[0].UUID, username: player.username, kills: FFAPlayer[0].kills, deaths: FFAPlayer[0].deaths }});
-});
-
-router.get('/canditature', async (req, res) => {
-    res.render('bewerbung', { userCount: await players.countDocuments() })
-});
-
-router.get('/complaint', async (req, res) => {
-    res.render('beschwerde', { userCount: await players.countDocuments() })
-});
-
-router.get('/about', async (req, res) => {
-    res.render('über', { userCount: await players.countDocuments() })
-});
-
-router.get('/p/:username', async (req, res) => {
-    const username = req.params.username;
-    const player = await players.findOne({ username: username });
-    if (player === null)
-        return res.render('profil', { userCount: await players.countDocuments(), error: 'notFound' });
-
-    const playerFFA = await playersFFA.findOne({ UUID: player.UUID });
-    if (playerFFA === null)
-        return res.render('profil', { userCount: await players.countDocuments(), profil: { uuid: player.UUID, username: username, banned: player.banned, group: player.perm_group } });
-    
-    const FFAData = { kills: playerFFA.kills, deaths: playerFFA.deaths };
-    const profilData = { uuid: player.UUID, username: username, banned: player.banned, group: player.perm_group, ffa: FFAData };
-
-    res.render('profil', { userCount: await players.countDocuments(), profil: profilData });
-});
-
-// Extras
-router.use(async (req, res, next) => {
-    res.status(404).render('error', { userCount: await players.countDocuments(), title: '404', msg: 'Diese Seite existiert nicht!' });
-});
-
-app.use("/", router);
+// Router
+app.use("/", getRouter);
---
+++ Web/routes/get.js
@@ -0,0 +1,56 @@
+const express = require('express'), router = express.Router(), players = require('../models/players'), playersFFA = require('../models/ffa');
+
+router.get('/', async (req, res) => {
+    res.render('index', { userCount: await players.countDocuments() });
+});
+
+router.get('/team', async (req, res) => {
+    res.render('team', { userCount: await players.countDocuments() });
+});
+
+router.get('/top', async (req, res) => {
+    const FFAPlayer = await playersFFA.find().sort({ kills: -1 }).limit(1);
+    const player = await players.findOne({ UUID: FFAPlayer[0].UUID });
+
+    if (FFAPlayer === null || player === null) {
+        res.render('topPlayers', { userCount: await players.countDocuments() })
+        return;
+    }
+
+    res.render('topPlayers', { userCount: await players.countDocuments(), ffa: { uuid: FFAPlayer[0].UUID, username: player.username, kills: FFAPlayer[0].kills, deaths: FFAPlayer[0].deaths }});
+});
+
+router.get('/canditature', async (req, res) => {
+    res.render('bewerbung', { userCount: await players.countDocuments() })
+});
+
+router.get('/complaint', async (req, res) => {
+    res.render('beschwerde', { userCount: await players.countDocuments() })
+});
+
+router.get('/about', async (req, res) => {
+    res.render('über', { userCount: await players.countDocuments() })
+});
+
+router.get('/p/:username', async (req, res) => {
+    const username = req.params.username;
+    const player = await players.findOne({ username: username });
+    if (player === null)
+        return res.render('profil', { userCount: await players.countDocuments(), error: 'notFound' });
+
+    const playerFFA = await playersFFA.findOne({ UUID: player.UUID });
+    if (playerFFA === null)
+        return res.render('profil', { userCount: await players.countDocuments(), profil: { uuid: player.UUID, username: username, banned: player.banned, group: player.perm_group } });
+    
+    const FFAData = { kills: playerFFA.kills, deaths: playerFFA.deaths };
+    const profilData = { uuid: player.UUID, username: username, banned: player.banned, group: player.perm_group, ffa: FFAData };
+
+    res.render('profil', { userCount: await players.countDocuments(), profil: profilData });
+});
+
+// Extras
+router.use(async (req, res, next) => {
+    res.status(404).render('error', { userCount: await players.countDocuments(), title: '404', msg: 'Diese Seite existiert nicht!' });
+});
+
+module.exports = router;
