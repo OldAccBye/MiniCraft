f2bee93da3df58522db078cbdcbe1550c4aca8ad
Nick <44214829+EntenKoeniq@users.noreply.github.com>
2021-02-16 19:11:28 +0100

    [SMALL] Web UPDATE

--- Web/logger.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs'), infoStream = fs.createWriteStream('./logs/info.txt');
-
-const Logger = (msg) => {
-    console.log(msg);
-    infoStream.write(new Date().toISOString() + " : " + msg + "\n")
-};
-
-module.exports = Logger;
